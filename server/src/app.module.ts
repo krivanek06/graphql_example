68bb8bcd6f180f118ea1ef05441311d0dea8e014
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-08-04 18:56:18 +0200

    first commit - init client & server

---
+++ server/src/app.module.ts
@@ -0,0 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
